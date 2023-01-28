fbfc511f46355b0885509b2e3d395dff7b0144d8
taiska06 <taiska06@gmail.com>
2023-01-28 21:14:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    double sum = 2000;
+    TextView Balanse ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Balanse =findViewById(R.id.textView);
+        MyThread minus = new MyThread("-");
+        minus.start();
+        MyThread plus=  new MyThread("+");
+        plus.start();
+
+    }
+
+    class MyThread extends Thread{
+        private String notification;
+
+        public MyThread(String notification) {
+            this.notification = notification;
+        }
+        @Override
+        public void run(){
+            double a=10000;
+            for (int i = 0; i < a; i++) {
+                if (Objects.equals(notification, "+")) {
+                    sum+= 1000*(Math.random());
+                }
+                if (Objects.equals(notification, "-")) {
+                    sum-= 1000*(Math.random());
+                }
+                if (sum < 0 || sum==0) {
+                    sum=0;
+                    bankrupt();
+                }
+                else {
+                    richman();
+                }
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+        public void bankrupt() {
+            Balanse.setText("У вас недостаточно средств");
+        }
+        public void richman() {
+            Balanse .setText("баланс = " + sum + "рублей");
+        }
+
+    }
+}
+
